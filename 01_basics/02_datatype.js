42f50f5346d58582cdffd575417b02a5867fd9e7
raazydv001 <143242259+raazydv001@users.noreply.github.com>
2023-12-01 17:08:09 +0000

    dataType

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(4+8) - we are using node.js not browser
+
+//Datatype
+//number
+//string
+//symbol
+//boolean
+//bigInt
+
+let name="rajkumar"//string 
+// null  is typeof object 
+// undifined is typrof undifined
