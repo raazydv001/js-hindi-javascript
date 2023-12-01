88bcec6c1231988308d55008d387fa4883e778d5
raazydv001 <143242259+raazydv001@users.noreply.github.com>
2023-12-01 17:07:47 +0000

    data type conversion

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,13 @@
+let score ="88ert"
+console.log(typeof score);
+let inNumber=Number(score)
+console.log(typeof inNumber);//Number
+console.log(inNumber);//NaN
+
+let isLoggedIn='';
+let checkBoolean=Boolean(isLoggedIn)
+console.log(checkBoolean);
+let someNum=44
+let someString=String(someNum);
+console.log(someString);
+console.log(typeof someString);
