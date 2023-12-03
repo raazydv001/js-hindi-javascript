8f6b3131bd3e954a597eff56ad61d2fea92944bd
raazydv001 <143242259+raazydv001@users.noreply.github.com>
2023-12-03 13:31:44 +0000

    dataType sumamry

---
+++ 01_basics/04_comparisonDatatypes.js
@@ -0,0 +1,11 @@
+console.log("3"> 2)
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0)// always avoid this  type of comparison
+console.log(undefined == 0)
+
+// strict check
+console.log("3" === 3);
---
+++ 01_basics/dataType-summary.js
@@ -0,0 +1,26 @@
+// primitive dataType
+
+// 7-types:String,Number,Boolean,null,undefined,symbol,BigInt
+
+let id=Symbol('123')
+let anotherId=Symbol('123')
+console.log(id===anotherId);
+
+
+let bigNumber=57863487573485n
+
+//Reference(Non-primitive) -- Array,Object,Function
+
+const heros=["shaktimaan","naagraj","doga"];
+
+let myObj={
+    name:"Raj",
+    age:20,
+
+}
+
+const myFunction=function(){
+    console.log("Hello World")
+}
+
+
